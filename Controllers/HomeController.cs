aaddaa872c8849068a80de1c1e0fdbe76efe2c7d
Ambrose Boo <ambroseboo@Ambroses-MacBook-Air.local>
2023-03-14 21:37:05 +0800

    Add select and send meeting invite feature

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,10 @@ using active_directory_aspnetcore_webapp_openidconnect_v2.Models;
 using System;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
 {
@@ -17,10 +21,27 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private readonly GraphServiceClient _graphServiceClient;
+        private readonly Microsoft.Graph.GraphServiceClient _graphServiceClient;
+
+        // Values from app registration
+        private static readonly string tenant_id = "dc6d3122-6e4f-4b41-817a-023f598a7169";
+        private static readonly string client_id = "b8f4001d-83a5-40c5-9d74-706d5c26aa25";
+        private static readonly string client_secret = "tXe8Q~R9yfr68iq25zhLFwLpBUQhkiA1V-CvBb-7";
+
+        // Get bearer token
+        private readonly string baseAddress = $"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token";
+        private static readonly string grant_type = "client_credentials";
+        private static readonly string[] scopes = new string[]{"https://graph.microsoft.com/.default"};
+
+        private readonly Dictionary<string, string> form = new Dictionary<string, string> {
+            {"grant_type", grant_type},
+            {"client_id", client_id},
+            {"client_secret", client_secret},
+            {"scope", scopes[0]}
+        };
 
         public HomeController(ILogger<HomeController> logger,
-                          GraphServiceClient graphServiceClient) {
+                          Microsoft.Graph.GraphServiceClient graphServiceClient) {
              _logger = logger;
             _graphServiceClient = graphServiceClient;
        }
@@ -29,6 +50,7 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
         public async Task<IActionResult> Index() {
             var user = await _graphServiceClient.Me.Request().GetAsync();
             ViewData["ApiResult"] = user.DisplayName;
+            TempData["UserPrincipalName"] = user.UserPrincipalName;
 
             return View();
         }
@@ -43,14 +65,18 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
             // create attendeeBase list
             string[] attendeeList = attendees.Split(',');
             System.Collections.Generic.List<AttendeeBase> attendeeBaseList = new System.Collections.Generic.List<AttendeeBase>();
+            HttpClient httpClient = await createHttpClientWithAuth();
+
             foreach (var attendeeEmail in attendeeList) {
-                // var attendeeUser = await _graphServiceClient.Users["{attendeeEmail}"].GetAsync();
-                // var attendeeName = attendeeUser["displayName"];
+                // get display name
+                var path = $"https://graph.microsoft.com/v1.0/users/{attendeeEmail}?$select=displayName";
+                HttpResponseMessage graphResponse = await httpClient.GetAsync(path);
+
                 attendeeBaseList.Add(
                     new AttendeeBase {
                         EmailAddress = new EmailAddress {
                             Address = attendeeEmail,
-                            Name = "attendeeName",
+                            Name = await graphResponse.Content.ReadAsStringAsync()
                         },
                         Type = AttendeeType.Required,
                     }
@@ -67,11 +93,11 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
                     new TimeSlot {
                         Start = new DateTimeTimeZone {
                             DateTime = startDateTime.ToString("s"),
-                            TimeZone = "Pacific Standard Time",
+                            TimeZone = "Singapore Standard Time",
                         },
                         End = new DateTimeTimeZone{
                             DateTime = endDateTime.ToString("s"),
-                            TimeZone = "Pacific Standard Time",
+                            TimeZone = "Singapore Standard Time",
                         },
                     },
                 },
@@ -103,6 +129,7 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
 
             TempData["meetingTimeSuggestions"] = JsonConvert.SerializeObject(result.MeetingTimeSuggestions);
             TempData["attendees"] = JsonConvert.SerializeObject(attendeeBaseList);
+            TempData["attendeesString"] = attendees;
             TempData["subject"] = subject;
             TempData["body"] = body;
             TempData["location"] = location;
@@ -114,6 +141,58 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
             return View(new SelectMeetingTimeModel { MeetingTimeSuggestions = JsonConvert.DeserializeObject<System.Collections.Generic.IEnumerable<Microsoft.Graph.MeetingTimeSuggestion>>((string)TempData["meetingTimeSuggestions"]) });
         }
 
+        public async Task<IActionResult> Select(string id, string id2) {
+            TempData["start"] = id.Replace("%2F", "/");
+            TempData["end"] = id2.Replace("%2F", "/");
+
+            Console.WriteLine(TempData["start"]);
+            Console.WriteLine(TempData["end"]);
+
+            // get attendees as list of Attendee
+            var attendees = JsonConvert.DeserializeObject<System.Collections.Generic.List<AttendeeBase>>((string)TempData["attendees"]);
+            System.Collections.Generic.List<Attendee> attendeeList = new System.Collections.Generic.List<Attendee>();
+            foreach (var attendeeBase in attendees) {
+                attendeeList.Add(
+                    new Attendee {
+                        EmailAddress = attendeeBase.EmailAddress,
+                        Type = AttendeeType.Required
+                    }
+                );
+            }
+
+            // create request body for post request
+            var requestBody = new Event {
+                Subject = (string)TempData["subject"],
+                Body = new ItemBody {
+                    ContentType = BodyType.Html,
+                    Content = (string)TempData["body"],
+                },
+                Start = new DateTimeTimeZone {
+                    DateTime = (string)TempData["start"],
+                    TimeZone = "Singapore Standard Time",
+                },
+                End = new DateTimeTimeZone {
+                    DateTime = (string)TempData["end"],
+                    TimeZone = "Singapore Standard Time",
+                },
+                Location = new Location {
+                    DisplayName = (string)TempData["location"],
+                },
+                Attendees = attendeeList,
+                AllowNewTimeProposals = false,
+            };
+
+            string json = JsonConvert.SerializeObject(requestBody);
+            Console.WriteLine(json);
+            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var path = $"https://graph.microsoft.com/v1.0/users/{TempData["UserPrincipalName"]}/calendar/events";
+            HttpClient httpClient = await createHttpClientWithAuth();
+            HttpResponseMessage graphResponse = await httpClient.PostAsync(path, httpContent);
+            return View();
+        }
+
+
         public IActionResult NoMeetingTime() {
             return View();
         }
@@ -123,5 +202,35 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private async Task<HttpClient> createHttpClientWithAuth() {
+            var httpClient = new HttpClient();
+            HttpResponseMessage tokenResponse = await httpClient.PostAsync(baseAddress, new FormUrlEncodedContent(form));
+            var jsonContent = await tokenResponse.Content.ReadAsStringAsync();
+            Token token = JsonConvert.DeserializeObject<Token>(jsonContent);
+            var authTokenStr = token.AccessToken;
+
+            if (!tokenResponse.IsSuccessStatusCode) {
+                throw new HttpRequestException("Call to get Token with HttpClient failed.");
+            }
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authTokenStr);
+            return httpClient;
+        }
+    }
+
+    internal class Token
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
     }
 }
